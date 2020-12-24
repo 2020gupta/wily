b218dd1b38fe85fb80954b74507249c7febf09e9
2020gupta <68911348+2020gupta@users.noreply.github.com>
2020-12-24 22:18:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import Facebook from './screens/FaceBook'
+import Insta from './screens/Insta'
+export default class App extends React.Component {
+  render(){
+  return (
+  <AppContainer/>
+  );
+  }
+}
+const TabNavigator=createBottomTabNavigator({
+ Facebook:{screen:Facebook},
+ Insta:{screen:Insta}
+})
+const AppContainer=createAppContainer(TabNavigator)
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
